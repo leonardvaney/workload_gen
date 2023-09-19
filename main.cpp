f42a784edd8d58ff38ee7e989a930810d3db7138
leonard <leonard.vaney@epfl.ch>
2023-09-19 11:07:50 +0200

    Remove mutex, add set of predefined memory location to increment

--- main.cpp
+++ main.cpp
@@ -3,57 +3,62 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define THREADS 1
+#define THREADS 8
 #define TRANSACTIONS 50000000
 #define CELLS_POWER 10
-#define CELLS 1 << CELLS_POWER
+#define CELLS (1 << CELLS_POWER)
+#define TRANSACTION_SET_SIZE 10000
 
-struct lock_t {
-    pthread_mutex_t mutex;
-};
-
-struct cell_t {
-    struct lock_t lock;
-    int value;
-};
 
 struct coroutine_args {
     int total_transaction;
-    struct cell_t* cells; 
-};
+    int thread_number;
+    int* transaction_set;
+    int** cells;
+    };
 
 void* generate_workload(void* transaction_args) {
     struct coroutine_args* args = (struct coroutine_args*)transaction_args;
-
     
     for(int i = 0; i < args->total_transaction; ++i){
-        uint random_value = rand();
-        uint cell_number = (random_value << (32 - CELLS_POWER)) >> (32 - CELLS_POWER);
-        
-        pthread_mutex_lock(&((&args->cells[cell_number].lock)->mutex));
-        args->cells[cell_number].value = args->cells[cell_number].value + 1;
-        pthread_mutex_unlock(&((&args->cells[cell_number].lock)->mutex));
+        int cell = args->transaction_set[i%TRANSACTION_SET_SIZE];
+        args->cells[args->thread_number][cell] += 1;
     }
 }
 
 int main() {
 
     pthread_t handlers[THREADS];
-    struct cell_t all_cells[CELLS];
+    coroutine_args args[THREADS];
+    int** all_cells = (int**)malloc(sizeof(int*) * THREADS);
+
+    for(int i = 0; i < THREADS; ++i){
+        all_cells[i] = (int*)malloc(sizeof(int) * CELLS);
+        for(int j = 0; j < CELLS; ++j){
+            all_cells[i][j] = 0;
+        }
+    }
 
     srand((unsigned)time(NULL));
 
-    for(int i = 0; i < CELLS; ++i) {
-        all_cells[i].value = 0;
-        pthread_mutex_init(&((&all_cells[i].lock)->mutex), NULL);
+    for(int i = 0; i < THREADS; ++i) {
+        for(int j = 0; j < CELLS; ++j){
+            all_cells[i][j] = 0;
+        }
     }
 
-    struct coroutine_args args = { TRANSACTIONS/THREADS, all_cells};
+    int transaction[TRANSACTION_SET_SIZE];
+    for(int i = 0; i < TRANSACTION_SET_SIZE; ++i){
+        uint random_value = rand();
+        uint cell_number = random_value%CELLS;
+        transaction[i] = cell_number;
+    }
 
     clock_t begin = clock();
 
     for(int i = 0; i < THREADS; ++i) {
-        pthread_create(&handlers[i], NULL, generate_workload, (void*)&args);
+        args[i] = { TRANSACTIONS/THREADS, i, transaction, all_cells};        
+        pthread_create(&handlers[i], NULL, generate_workload, (void*)&args[i]);
     }
     
     for(int i = 0; i < THREADS; ++i) {
@@ -64,10 +69,14 @@ int main() {
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     
     uint total_value = 0;
-    for(int i = 0; i < CELLS; ++i){
-        total_value += all_cells[i].value;
-        //printf("%u \n", all_cells[i].value);
+    for(int i = 0; i < THREADS; ++i){
+        for(int j = 0; j < CELLS; ++j){
+            total_value += all_cells[i][j];
+        }
+        free(all_cells[i]);
     }
+    free(all_cells);
+    
     printf("Total value: %u \n", total_value);
     printf("Time spent: %f \n", time_spent);
 }
