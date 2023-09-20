8882e1369fd3a87b3c5c9ec1e176692f2085896c
leonard <leonard.vaney@epfl.ch>
2023-09-20 13:56:26 +0200

    Can modify values with command line args

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define THREADS 2
-#define CELLS_POWER 25
-#define CELLS (1 << CELLS_POWER)
-#define TRANSACTION_SET_SIZE 10000
+//#define THREADS 2
+//#define CELLS_POWER 25
+//#define CELLS (1 << CELLS_POWER)
+//#define TRANSACTION_SET_SIZE 10000
 
 
 struct coroutine_args {
+    uint transaction_set_size;
     uint* transaction_set;
     uint* cells;
     };
@@ -18,41 +19,51 @@ void* execute_workload(void* transaction_args) {
     struct coroutine_args* args = (struct coroutine_args*)transaction_args;
     
     for(uint i = 0;; ++i){
-        uint cell = args->transaction_set[i%TRANSACTION_SET_SIZE];
+        uint cell = args->transaction_set[i%args->transaction_set_size];
         args->cells[cell] += 1;
     }
 }
  
 
-int main() {
+int main(int argc, char **argv) {
 
-    pthread_t handlers[THREADS];
-    coroutine_args args[THREADS];
-    uint** transaction = (uint**)malloc(sizeof(uint*) * THREADS);
-    uint** all_cells = (uint**)malloc(sizeof(uint*) * THREADS);
+    //Extract command line arguments
+    if(argc != 4){
+        printf("Need 3 args");
+        return 0;
+    }
+    const uint threads = atoi(argv[1]);
+    const uint cells_size = atoi(argv[2]);
+    const uint transaction_set_size = atoi(argv[3]);
+    
+
+    pthread_t handlers[threads];
+    coroutine_args args[threads];
+    uint** transaction = (uint**)malloc(sizeof(uint*) * threads);
+    uint** all_cells = (uint**)malloc(sizeof(uint*) * threads);
     srand((unsigned)time(NULL));
 
     //Init memory
-    for(uint i = 0; i < THREADS; ++i){
-        all_cells[i] = (uint*)malloc(sizeof(uint) * CELLS);
-        for(uint j = 0; j < CELLS; ++j){
+    for(uint i = 0; i < threads; ++i){
+        all_cells[i] = (uint*)malloc(sizeof(uint) * cells_size);
+        for(uint j = 0; j < cells_size; ++j){
             all_cells[i][j] = 0;
         }
     }
 
     //Init transaction set
-    for(uint i = 0; i < THREADS; ++i){
-        transaction[i] = (uint*)malloc(sizeof(uint) * TRANSACTION_SET_SIZE);
-        for(uint j = 0; j < TRANSACTION_SET_SIZE; ++j){
+    for(uint i = 0; i < threads; ++i){
+        transaction[i] = (uint*)malloc(sizeof(uint) * transaction_set_size);
+        for(uint j = 0; j < transaction_set_size; ++j){
             uint random_value = rand();
-            uint cell_number = random_value%CELLS;
+            uint cell_number = random_value%cells_size;
             transaction[i][j] = cell_number;
         }
     }
 
     //Thread creation
-    for(uint i = 0; i < THREADS; ++i) {
-        args[i] = { transaction[i], all_cells[i] };        
+    for(uint i = 0; i < threads; ++i) {
+        args[i] = { transaction_set_size, transaction[i], all_cells[i] };        
         pthread_create(&handlers[i], NULL, execute_workload, (void*)&args[i]);
     }
 
@@ -68,8 +79,8 @@ int main() {
         clock_gettime(CLOCK_REALTIME, &now);
         double time_spent = (double)(now.tv_sec - begin.tv_sec);
         if(time_spent >= 1.0){
-            for(uint i = 0; i < THREADS; ++i){
-                for(uint j = 0; j < CELLS; ++j){
+            for(uint i = 0; i < threads; ++i){
+                for(uint j = 0; j < cells_size; ++j){
                     total_value += all_cells[i][j];
                 }
             }        
